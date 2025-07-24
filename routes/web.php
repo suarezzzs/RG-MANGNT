2b8401f1f54965bdb44ff5f06f5cb3d2654ae9a1
carlos magno <carmaso_@hotmail.com>
2025-07-24 20:35:38 -0300

    Feat: mysql connection, create models end migrations

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
+use Illuminate\Mail\Message;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    echo "RG MANGNT";
+Route::get('/email', function () {
+
+    Mail::raw("Mensagem de Teste de RG MANGNT", function(Message $message){
+        $message->to("teste@gmail.com")
+        ->subject("Bem-vindo ao RH MANGNT")
+        ->from("rg@gmail.com");
+    });
+
+    echo "Email enviado com sucesso.";
+
 });
 
