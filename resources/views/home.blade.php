5be4baf5851e7351c0779792f03021f89cb6c46b
carlos magno <carmaso_@hotmail.com>
2025-07-25 00:54:49 -0300

    feat(auth): Scaffold authentication module

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-layout-app page-title="Home">
+
+    <h1 class="text-center">DETRO DA APP</h1>
+
+</x-layout-app>
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view("/login", "auth.login");
+Route::middleware("auth")->group(function(){
+    Route::view("/home", "home");
+});
